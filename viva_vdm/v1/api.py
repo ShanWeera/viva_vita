330d3c7f42db83359447b640cdf6d216ccb6b590
Shan Tharanga <63629580+ShanWeera@users.noreply.github.com>
2023-01-05 09:58:18 +0800

    apis in progress

---
+++ viva_vdm/v1/api.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from .endpoints import job, results
+
+app = FastAPI(
+    title="ViTA RESTful API",
+    description="This is the RESTful API of ViTA",
+    contact={'name': 'Shan Tharanga', 'email': 'stwm2@student.london.ac.uk'},
+    version="1.0.0",
+)
+app.include_router(job.router)
+app.include_router(results.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
