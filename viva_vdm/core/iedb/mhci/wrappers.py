0d99a099084892072f7dadc2a314fa7174217e9d
Shan Tharanga <63629580+ShanWeera@users.noreply.github.com>
2023-12-30 11:59:35 +0000

    having noqa in the wrong place breaks things

--- viva_vdm/core/iedb/mhci/wrappers.py
+++ viva_vdm/core/iedb/mhci/wrappers.py
@@ -84,78 +84,78 @@ class MhcINetMhcPan(MHCIPredictorBase):
     def get_table_template(self):
         return """
 <group>
-{{ allele }} {{ seq_num | to_int }} {{ start | to_int }} {{ end | to_int }} {{ length | to_int }} {{ sequence }} {{ core }} {{ icore }} {{ ic50 | to_float }} {{ percentile | to_float }} # noqa: E501
+{{ allele }} {{ seq_num | to_int }} {{ start | to_int }} {{ end | to_int }} {{ length | to_int }} {{ sequence }} {{ core }} {{ icore }} {{ ic50 | to_float }} {{ percentile | to_float }}
 </group>
-"""
+"""  # noqa: E501
 
 
 class MhcIANN(MHCIPredictorBase):
     def get_table_template(self):
         return """
 <group>
-{{ allele }} {{ seq_num | to_int }} {{ start | to_int }} {{ end | to_int }} {{ length | to_int }} {{ sequence }} {{ ic50 | to_float }} {{ percentile | to_float }} # noqa: E501
+{{ allele }} {{ seq_num | to_int }} {{ start | to_int }} {{ end | to_int }} {{ length | to_int }} {{ sequence }} {{ ic50 | to_float }} {{ percentile | to_float }}
 </group>
-"""
+"""  # noqa: E501
 
 
 class MhcINetMhcPanEL(MhcINetMhcPan):
     def get_table_template(self):
         return """
 <group>
-{{ allele }} {{ seq_num | to_int }} {{ start | to_int }} {{ end | to_int }} {{ length | to_int }} {{ sequence }} {{ core }} {{ icore }} {{ score | to_float }} {{ percentile | to_float }} # noqa: E501
+{{ allele }} {{ seq_num | to_int }} {{ start | to_int }} {{ end | to_int }} {{ length | to_int }} {{ sequence }} {{ core }} {{ icore }} {{ score | to_float }} {{ percentile | to_float }}
 </group>
-"""
+"""  # noqa: E501
 
 
 class MhcIPickpocket(MHCIPredictorBase):
     def get_table_template(self):
         return """
 <group>
-{{ allele }} {{ seq_num | to_int }} {{ start | to_int }} {{ end | to_int }} {{ length | to_int }} {{ sequence }} {{ ic50 | to_float }} {{ percentile | to_float }} # noqa: E501
+{{ allele }} {{ seq_num | to_int }} {{ start | to_int }} {{ end | to_int }} {{ length | to_int }} {{ sequence }} {{ ic50 | to_float }} {{ percentile | to_float }}
 </group>
-"""
+"""  # noqa: E501
 
 
 class MhcISMM(MHCIPredictorBase):
     def get_table_template(self):
         return """
 <group>
-{{ allele }} {{ seq_num | to_int }} {{ start | to_int }} {{ end | to_int }} {{ length | to_int }} {{ sequence }} {{ ic50 | to_float }} {{ percentile | to_float }} # noqa: E501
+{{ allele }} {{ seq_num | to_int }} {{ start | to_int }} {{ end | to_int }} {{ length | to_int }} {{ sequence }} {{ ic50 | to_float }} {{ percentile | to_float }}
 </group>
-"""
+"""  # noqa: E501
 
 
 class MhcISMMPMBEC(MHCIPredictorBase):
     def get_table_template(self):
         return """
 <group>
-{{ allele }} {{ seq_num | to_int }} {{ start | to_int }} {{ end | to_int }} {{ length | to_int }} {{ sequence }} {{ ic50 | to_float }} {{ percentile | to_float }} # noqa: E501
+{{ allele }} {{ seq_num | to_int }} {{ start | to_int }} {{ end | to_int }} {{ length | to_int }} {{ sequence }} {{ ic50 | to_float }} {{ percentile | to_float }}
 </group>
-"""
+"""  # noqa: E501
 
 
 class MhcIConsensus(MHCIPredictorBase):
     def get_table_template(self):
         return """
 <group>
-{{ allele }} {{ seq_num | to_int }} {{ start | to_int }} {{ end | to_int }} {{ length | to_int }} {{ sequence }} {{ percentile | to_float }} {{ ignore }} {{ ignore }} {{ ignore }} {{ ignore }} {{ ignore }} {{ ignore }} # noqa: E501
+{{ allele }} {{ seq_num | to_int }} {{ start | to_int }} {{ end | to_int }} {{ length | to_int }} {{ sequence }} {{ percentile | to_float }} {{ ignore }} {{ ignore }} {{ ignore }} {{ ignore }} {{ ignore }} {{ ignore }}
 </group>
-"""
+"""  # noqa: E501
 
 
 class MhcINetMhcCons(MHCIPredictorBase):
     def get_table_template(self):
         return """
 <group>
-{{ allele }} {{ seq_num | to_int }} {{ start | to_int }} {{ end | to_int }} {{ length | to_int }} {{ sequence }} {{ ic50 | to_float }} {{ percentile | to_float }} # noqa: E501
+{{ allele }} {{ seq_num | to_int }} {{ start | to_int }} {{ end | to_int }} {{ length | to_int }} {{ sequence }} {{ ic50 | to_float }} {{ percentile | to_float }}
 </group>
-"""
+"""  # noqa: E501
 
 
 class MhcINetMhcStabPan(MHCIPredictorBase):
     def get_table_template(self):
         return """
 <group>
-{{ allele }} {{ seq_num | to_int }} {{ start | to_int }} {{ end | to_int }} {{ length | to_int }} {{ sequence }} {{ ic50 | to_float }} {{ percentile | to_float }} # noqa: E501
+{{ allele }} {{ seq_num | to_int }} {{ start | to_int }} {{ end | to_int }} {{ length | to_int }} {{ sequence }} {{ ic50 | to_float }} {{ percentile | to_float }}
 </group>
-"""
+"""  # noqa: E501
